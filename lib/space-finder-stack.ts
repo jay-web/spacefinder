591bf47694a6c88fac524a4d8cf39c5be29a2978
jay-web <jay.developer12@gmail.com>
2022-11-03 12:08:29 +0530

    Added dynamoDB table creation class and integrate it into SpaceFinderStack

--- lib/space-finder-stack.ts
+++ lib/space-finder-stack.ts
@@ -4,7 +4,16 @@ import { Construct } from 'constructs';
 import {join} from 'path';
 import {Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { GenericTable } from './genericTable';
+
 export class SpaceFinderStack extends cdk.Stack {
+  // todo: Create instance of restapi from aws gateway api
+  private api = new RestApi(this, 'SpaceFinderApi');
+
+  // todo: Create Dynamo DB Table
+  private spaceFinderTable = new GenericTable("SpaceFinderTable", "SpaceFinder", this);
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -14,6 +23,13 @@ export class SpaceFinderStack extends cdk.Stack {
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main'
     })
+
+    // todo: hello api integration with lambda service
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource =  this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+
+
 
   }
 }
