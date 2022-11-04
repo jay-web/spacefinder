f2e0ac0b0a5a5d075a57a7bf9ad068c5df26f6f4
jay-web <jay.developer12@gmail.com>
2022-11-04 13:21:38 +0530

    Created debug config file to debug and test the code locally, also added aws-sdk to testing purpose only and added role policy to lambda function to access s3 buckets list

--- lib/space-finder-stack.ts
+++ lib/space-finder-stack.ts
@@ -7,6 +7,7 @@ import {Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { GenericTable } from './genericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class SpaceFinderStack extends cdk.Stack {
   // todo: Create instance of restapi from aws gateway api
@@ -19,21 +20,28 @@ export class SpaceFinderStack extends cdk.Stack {
     super(scope, id, props);
 
     // todo: Create lambda function
-    const helloLambda = new LambdaFunction(this, 'helloLambda', {
-      runtime: Runtime.NODEJS_16_X,
-      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-      handler: 'hello.main'
-    })
+    // const helloLambda = new LambdaFunction(this, 'helloLambda', {
+    //   runtime: Runtime.NODEJS_16_X,
+    //   code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+    //   handler: 'hello.main'
+    // })
 
     // todo: Create node lambda function 
-
     const helloNodeLambda = new NodejsFunction(this, "helloNodeLambda", {
       entry: join(__dirname, '..', 'services', 'node-lambdas', 'hello.ts'),
       handler: 'handler'
-    })
+    });
+
+    // todo: Create new policy statement to add s3 list bucket action to out lambda function
+    const s3listPolicy = new PolicyStatement();
+    s3listPolicy.addActions('s3:ListAllMyBuckets');
+    s3listPolicy.addResources('*');
+
+    // todo: Attach s3listPolicy to lambda function finally
+    helloNodeLambda.addToRolePolicy(s3listPolicy);
 
     // todo: hello api integration with lambda service
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaIntegration = new LambdaIntegration(helloNodeLambda);
     const helloLambdaResource =  this.api.root.addResource('hello');
     helloLambdaResource.addMethod('GET', helloLambdaIntegration);
 
