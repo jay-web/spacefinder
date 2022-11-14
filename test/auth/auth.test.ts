53b7481048f52dc68a95691fd717518f0bfd54a7
jay-web <jay.developer12@gmail.com>
2022-11-14 13:48:27 +0530

    Added getTempCredentialsFunction in authService class for testing purpose using debug and call the same in auth.test file, which run successfully

--- test/auth/auth.test.ts
+++ test/auth/auth.test.ts
@@ -1,7 +1,18 @@
 import {AuthService} from "./authService";
 import { Config } from "./config";
+import * as AWS from "aws-sdk";
 
 
-const authService = new AuthService();
+async function callJWTToken(){
+    const authService = new AuthService();
 
-const user = authService.login(Config.TEST_USER_NAME, Config.TEST_USER_PASSWORD)
+    const user = await authService.login(Config.TEST_USER_NAME, Config.TEST_USER_PASSWORD);
+    await authService.getAWSTempCredentials(user);
+    const someCredentials = AWS.config.credentials;
+    const a = 5;
+
+
+}
+
+
+callJWTToken();
--- test/auth/authService.ts
+++ test/auth/authService.ts
@@ -1,7 +1,8 @@
 import  {Auth, Amplify } from 'aws-amplify';
 import {Config} from "./config";
-
+import * as AWS from "aws-sdk";
 import { CognitoUser } from '@aws-amplify/auth';
+import { Credentials } from 'aws-sdk';
 
 Amplify.configure({
     Auth:{
@@ -19,4 +20,33 @@ export class AuthService {
         const user = await Auth.signIn(username, password) as CognitoUser;
         return user;
     }
+
+    public async getAWSTempCredentials(user: CognitoUser) {
+        const cognitoIdentityPool = `cognito-idp.${Config.REGION}.amazonaws.com/${Config.USER_POOL_ID}`;
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: Config.USER_IDENTITY_POOL_ID,
+            Logins:{
+                [cognitoIdentityPool]: user.getSignInUserSession()!.getIdToken().getJwtToken()
+            }
+        },
+        {
+            region: Config.REGION
+        })
+
+        await this.refreshCredentials();
+    }
+
+    private async refreshCredentials(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            (AWS.config.credentials as Credentials).refresh((error) => {
+                if(error){
+                    reject(error);
+                }else{
+                    resolve()
+                }
+            })
+        })
+    }
+
+
 }
