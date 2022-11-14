576dfa1e5043033e4a152b0dd0aeeea353ca9e9c
jay-web <jay.developer12@gmail.com>
2022-11-14 14:05:10 +0530

    Added normal test user from testing on normal user (not admin) and test the identity pool temp credentials on both users type to get s3 buckets list in auth.test file. Which run successfully

--- test/auth/auth.test.ts
+++ test/auth/auth.test.ts
@@ -1,14 +1,29 @@
 import {AuthService} from "./authService";
-import { Config } from "./config";
+import { Config, ConfigNormal } from "./config";
 import * as AWS from "aws-sdk";
 
+AWS.config.region = Config.REGION;
+
+// todo: this is just to get list of all buckets using identity pool temp credentials
+// ? if user is not authorized using s3 aws service from identity pool, so it will throw error
+async function getAllBuckets(){
+    let buckets;
+    try {
+        buckets = new AWS.S3().listBuckets().promise();
+    } catch (error) {
+        buckets = undefined;
+    }
+    return buckets;
+}
 
 async function callJWTToken(){
     const authService = new AuthService();
-
+    
+    // ? Change below Config to ConfigNormal to test with normal user instead of admin 
     const user = await authService.login(Config.TEST_USER_NAME, Config.TEST_USER_PASSWORD);
     await authService.getAWSTempCredentials(user);
     const someCredentials = AWS.config.credentials;
+    const buckets = await getAllBuckets();
     const a = 5;
 
 
